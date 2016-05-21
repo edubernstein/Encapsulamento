b08d4427bcbece61a6ed884bbcd2a35cd6c6d123
edubernstein <SarahB1976>
2016-05-21 15:09:16 -0300

    Inclusão do projeto Encapsulamento

---
+++ Encapsulamento/src/Teste.java
@@ -0,0 +1,18 @@
+public class Teste {
+	
+	public static void main(String[] args) {
+		Funcionario f = new Funcionario();
+		f.setNome("Eduardo Bernstein");
+		f.setSalario(2000);
+		System.out.println("Nome: " + f.getNome());
+		System.out.println("Salário: " + f.getSalario());
+		Conta c = new Conta();
+		c.setNumero(123);
+		c.setSaldo(540.12);
+		c.setLimite(100.0);
+		System.out.println("Número:" + c.getNumero());
+		System.out.println("Saldo:" + c.getSaldo());
+		System.out.println("Limite:" + c.getLimite());
+	}
+	
+}
