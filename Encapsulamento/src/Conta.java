92ece55ad3201bb8d2c36c847397dc7bf621135c
edubernstein <SarahB1976>
2016-05-21 16:11:07 -0300

    .

--- Encapsulamento/src/Conta.java
+++ Encapsulamento/src/Conta.java
@@ -3,47 +3,26 @@ public class Conta {
 	private double	saldo;
 	private double	limite;
 	
-	/**
-	 * @return the numero
-	 */
 	public int getNumero() {
 		return numero;
 	}
 	
-	/**
-	 * @param numero
-	 *            the numero to set
-	 */
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
 	
-	/**
-	 * @return the saldo
-	 */
 	public double getSaldo() {
 		return saldo;
 	}
 	
-	/**
-	 * @param saldo
-	 *            the saldo to set
-	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 	
-	/**
-	 * @return the limite
-	 */
 	public double getLimite() {
 		return limite;
 	}
 	
-	/**
-	 * @param limite
-	 *            the limite to set
-	 */
 	public void setLimite(double limite) {
 		this.limite = limite;
 	}
--- Encapsulamento/src/Funcionario.java
+++ Encapsulamento/src/Funcionario.java
@@ -1,27 +1,19 @@
 public class Funcionario {
 	private double	salario;
 	private String	nome;
-	/**
-	 * @return the salario
-	 */
+	
 	public double getSalario() {
 		return salario;
 	}
-	/**
-	 * @param salario the salario to set
-	 */
+	
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	/**
-	 * @return the nome
-	 */
+	
 	public String getNome() {
 		return nome;
 	}
-	/**
-	 * @param nome the nome to set
-	 */
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
